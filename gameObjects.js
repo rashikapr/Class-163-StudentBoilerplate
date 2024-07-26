64270388143522a84325932111ee1869c567baba
rashikapr <74289013+rashikapr@users.noreply.github.com>
2024-07-26 14:24:22 +0530

    Create Boxes

--- gameObjects.js
+++ gameObjects.js
@@ -82,41 +82,5 @@ AFRAME.registerComponent("wire-fence", {
 });
 
 //boxes
-AFRAME.registerComponent("boxes", {
-  schema: {
-    height: { type: "number", default: 3 },
-    width: { type: "number", default: 3 },
-    depth: { type: "number", default: 3 },
-  },
-  init: function () {
-    for (var i = 0; i < 20; i++) {
-      var box = document.createElement("a-entity");
-      box.setAttribute("id", "box" + i); 
-
-      posX = Math.random()*200 -100;
-      posY = 1.5;
-      posZ =Math.random()*200 -100;
-
-      position = { x: posX, y: posY, z: posZ };           
-      box.setAttribute("position", position);
-
-      box.setAttribute("geometry", {
-        primitive: "box",
-        height: this.data.height,
-        width: this.data.width,
-        depth: this.data.depth,
-      });
-
-      box.setAttribute("material", {
-        src: "./images/boxtexture1.jpg",
-        repeat: "1 1 1",
-      });
-
-      box.setAttribute("static-body", {});
-      var sceneEl = document.querySelector("#scene");
-      sceneEl.appendChild(box);
-    }
-  },
-});
 
 
